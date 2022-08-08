3e3af14b67d360aad3a127272d1c02367ed92e4a
Omar Scher <omar.sr88@gmail.com>
2022-08-08 15:17:57 -0400

    Adds param to youtube video to be tracked

--- src/components/DemoPage.js
+++ src/components/DemoPage.js
@@ -5,7 +5,7 @@ export default function DemoPage() {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/dxmd2Uy_fIM"
+        src="https://www.youtube.com/embed/dxmd2Uy_fIM?enablejsapi=1"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
