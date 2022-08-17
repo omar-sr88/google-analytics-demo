f448d27e334ad6d29b77e0db41a7b6ca2983a51a
Omar Scher <omar.sr88@gmail.com>
2022-08-17 16:50:13 -0400

    Sending event via button click

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import ReactGA from "react-ga";
 import "./App.css";
 import { Link } from "react-router-dom";
 
+ReactGA.initialize("UA-237915805-1");
+ReactGA.plugin.require("ec");
+
 function App() {
+  const handleButtonClick = () => {
+    ReactGA.plugin.execute("ec", "addProduct", {
+      id: 1,
+      name: "test product",
+      category: "fake test",
+      brand: "jane tests inc",
+      price: 30,
+      quantity: 2,
+    });
+    ReactGA.plugin.execute("ec", "setAction", "purchase", {
+      id: 1,
+      revenue: 60,
+    });
+    ReactGA.ga("send", "event", "UX", "click", "checkout success");
+  };
+
   return (
     <div className="App">
       <nav>
@@ -9,6 +29,7 @@ function App() {
         <Link to="/expenses">About us</Link>
       </nav>
       <div>Some landing page content</div>
+      <button onClick={handleButtonClick}>Send purchase event</button>
     </div>
   );
 }
